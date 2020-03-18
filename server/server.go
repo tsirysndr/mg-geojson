d9f53e9f783a637295a410b29d31a2285a3017b1
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2020-03-18 10:24:51 +0300

    HTTP serve all JSON files

--- server/server.go
+++ server/server.go
@@ -1,5 +1,71 @@
 package server
 
-func StartHTTPServer() {
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+const (
+	country   = "assets/country.json"
+	regions   = "assets/regions.json"
+	districts = "assets/districts.json"
+	communes  = "assets/communes.json"
+	fokontany = "assets/fokontany.json"
+)
+
+func StartHTTPServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/country", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadFile(country)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	})
+	mux.HandleFunc("/regions", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadFile(regions)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	})
+	mux.HandleFunc("/districts", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadFile(districts)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	})
+	mux.HandleFunc("/communes", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadFile(communes)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	})
+	mux.HandleFunc("/fokontany", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadFile(fokontany)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	})
+
+	s := &http.Server{
+		Addr:    ":8000",
+		Handler: mux,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
